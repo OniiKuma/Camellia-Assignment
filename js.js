de0515bccc19f1875978514a345a15ccacae35fa
OniiKuma <105169315+OniiKuma@users.noreply.github.com>
2022-12-08 23:39:07 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+//we tell it to function our function which is the alert
+function myFunction() { //openning scope operator
+    //we set the alert
+    alert('Crystalized - Camellia');
+    //we tell it to open the song in a tab when we click "Okay"
+    window.open('https://www.youtube.com/watch?v=dkYHgxfQZBA');
+}//closing scope operator
